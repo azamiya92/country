4389bfe4e746e3a461497035161406cacb9b4340
Mohammad Azam <Mohammad Azam>
2016-10-26 21:33:32 +0300

    mysql2

--- project/settings.py
+++ project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default' : {
         'ENGINE' : 'django.db.backends.mysql',
         'NAME' : 'country',
-        'USER' : 'root'.
+        'USER' : 'root',
         'PASSWORD' : 'azambhaiACCA2',
         'HOST' : '127.0.0.1',
         'PORT' : '' ,
